56514e8623c2aa3ac1fcd5414ed1645b4aa9f525
dcbyers13 <dcbyers13@gmail.com>
2026-07-16 19:35:05 -0500

    Remove unused TemplateView import from URLconf

--- config/urls.py
+++ config/urls.py
@@ -31,7 +31,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from apps.core.views import react_app_view
